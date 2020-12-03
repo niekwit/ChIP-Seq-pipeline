09c09220f48738496a88a626e74239cf3ce56ca4
niekwit <niekwit@gmail.com>
2020-12-03 12:10:08 +0000

    added bigWig file generation

--- chip-seq-paired.sh
+++ chip-seq-paired.sh
@@ -19,15 +19,23 @@ do
 	read2_fastq_gz="${read1_fastq_gz%_1.fastq.gz}_2.fastq.gz"
 	trim_galore -j 4 -o ./trim_galore --paired $read1_fastq_gz $read2_fastq_gz
 	read1_val_1_fq_gz="${read1_fastq_gz%_1.fastq.gz}_1_val_1.fq.gz"
-	read1_val_1_fq_gz="$trim_galore/{read1_val_1_fq_gz##*/}"
-	read2_val_2_fq_gz="${read1_fastq_gz%_2.fastq.gz}_2_val_2.fq.gz"
-	read2_val_2_fq_gz="$trim_galore/{read2_val_2_fq_gz##*/}"
+	read1_val_1_fq_gz="trim_galore/${read1_val_1_fq_gz##*/}"
+	read2_val_2_fq_gz="${read2_fastq_gz%_2.fastq.gz}_2_val_2.fq.gz"
+	read2_val_2_fq_gz="trim_galore/${read2_val_2_fq_gz##*/}"
 	bowtie2_output=${read1_fastq_gz%_1.fastq.gz}
 	bowtie2_output=${bowtie2_output##*/}
 	bowtie2 -p 40 -x $index_path -1 $read1_val_1_fq_gz -2 $read2_val_2_fq_gz 2>> chip-seq.log | samtools view -q 15 -bS - > "bam/${bowtie2_output}.bam" 2>> chip-seq.log
 	bedtools intersect -v -a "bam/${bowtie2_output}.bam" -b $blacklist_path > "bam/${bowtie2_output}-bl.bam" -nonamecheck 2>> chip-seq.log #removes reads from BAM file that are blacklisted
 	java -jar $PICARD SortSam INPUT="bam/${bowtie2_output}-bl.bam" OUTPUT="bam/${bowtie2_output}-sort-bl.bam" SORT_ORDER=coordinate 2>> chip-seq.log
 	java -jar $PICARD MarkDuplicates INPUT="bam/${bowtie2_output}-sort-bl.bam" OUTPUT="bam/${bowtie2_output}-dedupl-sort-bl.bam" REMOVE_DUPLICATES=TRUE METRICS_FILE=metric.txt 2>> chip-seq.log
+	samtools_input="bam/${bowtie2_output}-dedupl-sort-bl.bam"
+	samtools index -@ 40 -b $samtools_input 2>> chip-seq.log
+	
+	bam_input="bam/${bowtie2_output}-dedupl-sort-bl.bam"
+	bigwig_output="${samtools_input%-dedupl-sort-bl.bam}-norm.bw"
+	bigwig_output=${bigwig_output##*/}
+	bamCoverage -p 40 --normalizeUsing RPKM -b $bam_input -o bigwig/$bigwig_output 2>> chip-seq.log
+	
 done
 
 
