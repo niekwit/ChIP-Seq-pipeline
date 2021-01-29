e0512e269c77f66682622ad9b6cb44d7baee918b
niekwit <41302859+niekwit@users.noreply.github.com>
2021-01-29 16:03:11 +0000

    added ngs.plot

--- chip-seq-pipeline.sh
+++ chip-seq-pipeline.sh
@@ -100,6 +100,7 @@ then
 		echo "$file: $count" >> mapped_read_count_no_dedup.txt
 	done
 	echo "Alignment completed. Check if deduplication and/or downsampling is required."	
+	#add code thats can suggest if samples need downsampling
 	if [[ $# == 2 ]];
 		then
 			exit 0
@@ -213,7 +214,7 @@ fi
 
 if [[ $@ == *"ngsplot"* ]];
 then
-    	echo "Generating metagene plots and heatmaps with ngs.plot"
+    	echo "Generating metagene plots and heatmaps with ngs.plot.r"
 	mkdir ngsplot 
 	if [[ -f homer_hg19 ]]; 
 		then
@@ -229,11 +230,26 @@ then
 			*input*) continue;;
 			*) 
 				ngs_output=${file%-dedupl-sort-bl.bam}
-				ngs_output=${ngs_output##*/}	
-				ngs.plot.r -G $ngsplot_genome -R tss -C $file -O "ngsplot/${ngs_output}_tss" -T "ngsplot/$ngs_output" -L 5000
+				ngs_output=${ngs_output##*/}
+				mkdir "ngsplot/$ngs_output"
+				ngs.plot.r -G $ngsplot_genome -R tss -C $file -O "ngsplot/$ngs_output/${ngs_output}_tss" -T $ngs_output -L 5000
 		esac
 	done
 	
+	if [[ -f ngsplot.conf ]]; 
+	then
+		sed '1d' ngsplot.conf > ngsplot-temp.conf #removes header
+		input="ngsplot-temp.conf"
+		while IFS= read -r line
+		do
+			ngsplot_chip=$(echo $line | cut -d "," -f 1)
+			ngsplot_input=$(echo $line | cut -d "," -f 2)
+			ngsplot_output_name=$(echo $line | cut -d "," -f 3)
+			mkdir "ngsplot/$ngsplot_output_name"
+			ngs.plot.r -G $ngsplot_genome -R tss -C "$ngsplot_chip:$ngsplot_input" -O "ngsplot/$ngsplot_output_name/${ngsplot_output_name}_tss" -T $ngsplot_output_name -L 5000
+		done < "$input"
+	fi
+	rm ngsplot-temp.conf *.cnt
 	if [[ $# == 1 ]];
 		then
     			exit 0
@@ -245,6 +261,7 @@ then
     	echo "QC analysis ChIP-Seq data"
 	mkdir chip-qc
 	
+	plotCorrelation
 	if [[ $# == 1 ]];
 		then
     			exit 0
