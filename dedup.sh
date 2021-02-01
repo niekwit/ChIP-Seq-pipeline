dfe1455e809c46ff035c37dd0e28f8ca500a1293
niekwit <41302859+niekwit@users.noreply.github.com>
2021-02-01 16:57:56 +0000

    simplified

--- dedup.sh
+++ dedup.sh
@@ -2,17 +2,14 @@
 
 dedup() {
 echo "Performing deduplication"
-	for file in bam/*-bl.bam
+	echo "Removing duplicates $file"
+	
+	for file in bam/*-sort-bl.bam
 	do
-		echo "Removing duplicates $file"
-		sort_output=${file%-bl.bam}
-		sort_output="$sort_output-sort-bl.bam"
-		java -jar $PICARD SortSam INPUT=$file OUTPUT=$sort_output SORT_ORDER=coordinate 2>> deduplication.log
-		dedup_output=${file%-bl.bam}
+		dedup_output=${file%-sort-bl.bam}
 		dedup_output="$dedup_output-dedupl-sort-bl.bam"
-		java -jar $PICARD MarkDuplicates INPUT=$sort_output OUTPUT=$dedup_output REMOVE_DUPLICATES=TRUE METRICS_FILE=metric.txt 2>> deduplication.log
-	done	
-	#count mapped reads after deduplication:	
+		java -jar $PICARD MarkDuplicates INPUT=$file OUTPUT=$dedup_output REMOVE_DUPLICATES=TRUE METRICS_FILE=metric.txt 2>> deduplication.log
+	done
 	echo "Uniquely mapped read counts after deduplication:" >> mapped_read_count_dedup.txt	
 	for file in bam/*dedupl-sort-bl.bam
 	do
