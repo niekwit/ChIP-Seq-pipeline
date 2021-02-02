747d9be6956a1d5ca47a4261f8d84f875cb15ed3
niekwit <41302859+niekwit@users.noreply.github.com>
2021-02-02 11:26:09 +0000

    included bwa

--- align.sh
+++ align.sh
@@ -16,22 +16,38 @@ if [[ $* == *"hisat"* ]];
 		touch homer_mm9 #file for loading correct genome with downstream applications
 		echo "Mouse genome (mm9) and blacklist selected"
 	fi
-	
 	mkdir -p {bam,trim_galore}
-	for read1_fastq_gz in raw-data/*_1.fastq.gz
-	do
-		read2_fastq_gz="${read1_fastq_gz%_1.fastq.gz}_2.fastq.gz"
-		echo "Trimming adapter sequences $read1_fastq_gz"
-		trim_galore -j 4 -o ./trim_galore --paired $read1_fastq_gz $read2_fastq_gz 2>> align.log
-		read1_val_1_fq_gz="${read1_fastq_gz%_1.fastq.gz}_1_val_1.fq.gz"
-		read1_val_1_fq_gz="trim_galore/${read1_val_1_fq_gz##*/}"
-		read2_val_2_fq_gz="${read2_fastq_gz%_2.fastq.gz}_2_val_2.fq.gz"
-		read2_val_2_fq_gz="trim_galore/${read2_val_2_fq_gz##*/}"
-		hisat2_output=${read1_fastq_gz%_1.fastq.gz}
-		echo "Aligning reads to reference $read1_fastq_gz"
-		hisat2 -p $max_threads -x $index_path -1 $read1_val_1_fq_gz -2 $read2_val_2_fq_gz 2>> align.log | samtools view -q 15 -F 260 -bS -@ $max_threads - | bedtools intersect -v -a "stdin" -b $blacklist_path -nonamecheck | samtools sort -@ $max_threads - > "${hisat2_output}-sort-bl.bam"
-		echo "Removing blacklisted regions from alignment $read1_fastq_gz"
-	done	
+	
+	if [[ $* == *"hisat-pe"* ]];
+	then
+		echo "Paired-end alignment selected with HISAT2"
+		echo "Trimming and aligning fastq files"
+		for read1_fastq_gz in raw-data/*_1.fastq.gz
+		do
+			read2_fastq_gz="${read1_fastq_gz%_1.fastq.gz}_2.fastq.gz"
+			trim_galore -j 4 -o ./trim_galore --paired $read1_fastq_gz $read2_fastq_gz 2>> align.log
+			read1_val_1_fq_gz="${read1_fastq_gz%_1.fastq.gz}_1_val_1.fq.gz"
+			read1_val_1_fq_gz="trim_galore/${read1_val_1_fq_gz##*/}"
+			read2_val_2_fq_gz="${read2_fastq_gz%_2.fastq.gz}_2_val_2.fq.gz"
+			read2_val_2_fq_gz="trim_galore/${read2_val_2_fq_gz##*/}"
+			hisat2_output="${read1_fastq_gz%_1.fastq.gz}-sort-bl.bam"
+			hisat2_output="bam/${read1_fastq_gz##*/}"
+			hisat2 -p $max_threads -x $index_path -1 $read1_val_1_fq_gz -2 $read2_val_2_fq_gz 2>> align.log | samtools view -q 15 -F 260 -bS -@ $max_threads - | bedtools intersect -v -a "stdin" -b $blacklist_path -nonamecheck | samtools sort -@ $max_threads - > $hisat2_output #bam file will not contain unmapped and multimapped reads 
+		done
+	elif [[ $* == *"hisat-se"* ]];
+	then
+		echo "Single-end alignment selected with HISAT2"
+		echo "Trimming and aligning fastq files"
+		for file in raw-data/*.fastq.gz
+		do
+			trim_galore -j 4 -o ./trim_galore $file 2>> align.log #output extension will be "_trimmed.fq.gz"
+			hisat2_input="${file%.fastq.gz}_trimmed.fq.gz"
+			hisat2_input="trim_galore/${hisat2_input##*/}"
+			hisat2_output="bam/${file%_1.fastq.gz}-sort-bl.bam"
+			hisat2 -p $max_threads -x $index_path $hisat2_input 2>> align.log | samtools view -q 15 -F 260 -bS -@ $max_threads - | bedtools intersect -v -a "stdin" -b $blacklist_path -nonamecheck | samtools sort -@ $max_threads - > $hisat2_output #bam file will not contain unmapped and multimapped reads
+		done
+	fi
+	
 	echo "Uniquely mapped read counts before deduplication:" >> mapped_read_count_no_dedup.txt	
 	for file in bam/*-sort-bl.bam
 	do
@@ -39,41 +55,57 @@ if [[ $* == *"hisat"* ]];
 		echo "$file: $count" >> mapped_read_count_no_dedup.txt
 	done
 elif [[ $* == *"bwa"* ]]; ###not working yet###
-	then
-		then	
+	then	
 	if [[ $* == *"human"* ]];
-	then 
-		index_path="###" #hg19
+	then
+		index_path="/home/niek/Documents/references/bwa-index/GRCh37/ucsc.hg19.fasta" #hg19
 		blacklist_path="/home/niek/Documents/references/blacklists/Human/hg19/wgEncodeDukeMapabilityRegionsExcludable.bed" #hg19
 		touch homer_hg19 #file for loading correct genome with downstream applications
 		echo "Human genome (hg19) and blacklist selected"
 	elif [[ $* == *"mouse"* ]];
-	then	
+	then
 		index_path="###" #mm9
 		blacklist_path="/home/niek/Documents/references/blacklists/Mouse/mm9-blacklist.bed" #mm9
 		touch homer_mm9 #file for loading correct genome with downstream applications
 		echo "Mouse genome (mm9) and blacklist selected"
 	fi
-	
 	mkdir -p {bam,trim_galore}
-	for read1_fastq_gz in raw-data/*_1.fastq.gz
-	do
+	
+	if [[ $* == *"bwa-pe"* ]];
+	then
+		echo "Paired-end alignment selected with bwa"
+		echo "Trimming and aligning fastq files"
+		for read1_fastq_gz in raw-data/*_1.fastq.gz
+		do
+			read2_fastq_gz="${read1_fastq_gz%_1.fastq.gz}_2.fastq.gz"
+			trim_galore -j 4 -o ./trim_galore --paired $read1_fastq_gz $read2_fastq_gz 2>> align.log
+			read1_val_1_fq_gz="${read1_fastq_gz%_1.fastq.gz}_1_val_1.fq.gz"
+			read1_val_1_fq_gz="trim_galore/${read1_val_1_fq_gz##*/}"
+			read2_val_2_fq_gz="${read2_fastq_gz%_2.fastq.gz}_2_val_2.fq.gz"
+			read2_val_2_fq_gz="trim_galore/${read2_val_2_fq_gz##*/}"
+			bwa_output="${read1_fastq_gz%_1.fastq.gz}-sort-bl.bam"
+			bwa_output="bam/${read1_fastq_gz##*/}"
+			bwa mem $index_path -t $max_threads  $read1_val_1_fq_gz $read2_val_2_fq_gz | samtools view -q 15 -F 260 -bS -@ $max_threads - | bedtools intersect -v -a "stdin" -b $blacklist_path -nonamecheck | samtools sort -@ $max_threads - > $bwa_output #bam file will not contain unmapped and multimapped reads
+		done
+	elif [[ $* == *"bwa-se"* ]];
+	then
+		echo "Single-end alignment selected with bwa"
+		echo "Trimming and aligning fastq files"
+		for file in raw-data/*_1.fastq.gz
+		do
 		read2_fastq_gz="${read1_fastq_gz%_1.fastq.gz}_2.fastq.gz"
 		echo "Trimming adapter sequences $read1_fastq_gz"
-		trim_galore -j 4 -o ./trim_galore --paired $read1_fastq_gz $read2_fastq_gz 2>> align.log
-		read1_val_1_fq_gz="${read1_fastq_gz%_1.fastq.gz}_1_val_1.fq.gz"
-		read1_val_1_fq_gz="trim_galore/${read1_val_1_fq_gz##*/}"
-		read2_val_2_fq_gz="${read2_fastq_gz%_2.fastq.gz}_2_val_2.fq.gz"
-		read2_val_2_fq_gz="trim_galore/${read2_val_2_fq_gz##*/}"
-		hisat2_output=${read1_fastq_gz%_1.fastq.gz}
-		hisat2_output=${hisat2_output##*/}
-		echo "Aligning reads to reference $read1_fastq_gz"
-		#bwa mem -t $max_threads 
-		#bwa 		
-		#hisat2 -p $max_threads -x $index_path -1 $read1_val_1_fq_gz -2 $read2_val_2_fq_gz 2>> align.log | samtools view -q 15 -F 260 -bS -@ $max_threads - > "bam/${hisat2_output}.bam" 2>> align.log
-		echo "Removing blacklisted regions from alignment $read1_fastq_gz"
-		bedtools intersect -v -a "bam/${hisat2_output}.bam" -b $blacklist_path > "bam/${hisat2_output}-bl.bam" -nonamecheck 2>> align.log
-	done	
+		for file in raw-data/*.fastq.gz
+		do
+			trim_galore -j 4 -o ./trim_galore $file 2>> align.log #output extension will be "_trimmed.fq.gz"
+			bwa_input="${file%.fastq.gz}_trimmed.fq.gz"
+			bwa_input="trim_galore/${hisat2_input##*/}"
+			bwa_output="bam/${file%_1.fastq.gz}-sort-bl.bam"
+			bwa mem $index_path -t $max_threads 2>> align.log | samtools view -q 15 -F 260 -bS -@ $max_threads - | bedtools intersect -v -a "stdin" -b $blacklist_path -nonamecheck | samtools sort -@ $max_threads - > $bwa_output #bam file will not contain unmapped and multimapped reads
+		done
+	fi
+	
+	
 	#count mapped reads:	
 	echo "Uniquely mapped read counts before deduplication:" >> mapped_read_count_no_dedup.txt	
 	for file in bam/*bl.bam
